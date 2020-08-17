2b4c7912258f992bdcfa2295d42e0caf7cc2a172
MenuraSankalpa <sankalpadissanayaka99@gmail.com>
2020-08-17 05:37:32 +0530

    IT19216492 update his AddNewSupplier.java,OrderRequest.java,SupplierInterface.java,UpdateSupplierDetails.java,activity_supplier_interface.xml,activity_update_supplier_details.xml and fragment_requst_supply.xml

---
+++ app/src/main/java/com/teamevox/freshfred/OrderRequest.java
@@ -0,0 +1,14 @@
+package com.teamevox.freshfred;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_request);
+    }
+}
