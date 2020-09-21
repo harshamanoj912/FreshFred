16296ab948735a53098da9e47855efd9ad09c98c
Harsha Manoj Suraweera <54607738+harshamanoj912@users.noreply.github.com>
2020-09-21 16:45:24 +0530

    it19208718

---
+++ app/src/main/java/com/teamevox/freshfred/SupplierLogin.java
@@ -0,0 +1,14 @@
+package com.teamevox.freshfred;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SupplierLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supplier_login);
+    }
+}
