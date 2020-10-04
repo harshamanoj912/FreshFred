71c464d7aaa9f2aebb94bcc7550e905791045c89
Harsha Manoj Suraweera <54607738+harshamanoj912@users.noreply.github.com>
2020-10-04 22:39:38 +0530

    harsha - done

--- app/src/main/java/com/teamevox/freshfred/IT19208718/Calc.java
+++ app/src/main/java/com/teamevox/freshfred/IT19208718/Calc.java
@@ -1,5 +1,5 @@
 package com.teamevox.freshfred.IT19208718;
-
+//used for test cases
 public class Calc {
 
     public static double calculateCommission(double tmpVal, double totalPriceForTheOrder){
